6d9f86e5f9cd91e145bc4810bdf680b3dff45e3f
Rafael Gomes de Souza <rafael.souza@coopercard.com.br>
2019-08-06 18:04:41 -0300

    Melhorias na home e single de posts

--- projeto-4/src/Controller/PostController.php
+++ projeto-4/src/Controller/PostController.php
@@ -3,17 +3,23 @@ namespace Code\Controller;
 
 use Code\DB\Connection;
 use Code\Entity\Post;
+use Code\Session\Flash;
 use Code\View\View;
 
 class PostController
 {
     public function index($slug)
     {
-        $post = new Post(Connection::getInstance());
+        try {
+            $post = new Post(Connection::getInstance());
 
-        $view = new View('site' . DIRECTORY_SEPARATOR . 'single.phtml');
-        $view->post = current($post->where(['slug' => $slug]));
+            $view = new View('site' . DIRECTORY_SEPARATOR . 'single.phtml');
+            $view->post = current($post->where(['slug' => $slug]));
 
-        return $view->render();
+            return $view->render();
+        } catch (\Exception $e) {
+            Flash::add('warning', 'Postagem não encontrada!');
+            return header('Location: ' . HOME);
+        }
     }
 }
