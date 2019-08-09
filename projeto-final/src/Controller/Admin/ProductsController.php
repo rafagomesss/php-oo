eabb7c18f1bf8edf8e486bb3ee0d56e3bda98819
Rafael Gomes de Souza <rafael.souza@coopercard.com.br>
2019-08-09 15:32:05 -0300

    salvando produtos no banco de dados

--- projeto-final/src/Controller/Admin/ProductsController.php
+++ projeto-final/src/Controller/Admin/ProductsController.php
@@ -1,8 +1,10 @@
 <?php
 namespace Code\Controller\Admin;
 
+use Ausi\SlugGenerator\SlugGenerator;
 use Code\DB\Connection;
 use Code\Entity\Product;
+use Code\Session\Flash;
 use Code\View\View;
 
 class ProductsController
@@ -17,6 +19,22 @@ class ProductsController
 
     public function new()
     {
+
+        if (filter_input(INPUT_SERVER, 'REQUEST_METHOD') === 'POST') {
+            $data = filter_input_array(INPUT_POST, FILTER_SANITIZE_STRING);
+
+            $data['slug'] = (new SlugGenerator())->generate($data['name']);
+
+            $product = new Product(Connection::getInstance());
+
+            if(!$product->insert($data)) {
+                Flash::add('error', 'Erro ao criar produto!');
+                return header('Location: ' . HOME . '/admin/products/new');
+            }
+
+            Flash::add('success', 'Produto criado com sucesso!');
+            return header('Location: ' . HOME . '/admin/products');
+        }
         return (new View('admin' . DS . 'products' . DS . 'new.phtml'))->render();
     }
 
